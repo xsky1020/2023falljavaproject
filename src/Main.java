df34b80786a18c7dd71d0622f2eff20b5365690e
xsky1020 <12311020@mail.sustech.edu.cn>
2023-12-18 16:48:27 +0800

    路径

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import controller.GameController;
 import model.Chessboard;
+import model.Music;
 import view.ChessGameFrame;
 
 import javax.swing.*;
@@ -10,6 +11,11 @@ public class Main {
             ChessGameFrame mainFrame = new ChessGameFrame(1100, 810);
             GameController gameController = new GameController(mainFrame.getChessboardComponent(), new Chessboard(0), mainFrame.getScore(), mainFrame.getStepLeft());
             mainFrame.setVisible(true);
+
+            Music audioPlayWave = new Music("music/music.wav");// 开音乐(冒号里的内容与音乐文件名一致)
+            audioPlayWave.start();
+            @SuppressWarnings("unused")
+            int musicOpenLab = 1;
         });
     }
 }
